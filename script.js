566a490f4d70feab62d1d442e074cc44015bc5d6
deepikas19998-art <deepikas19998@gmail.com>
2025-08-26 22:41:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
-  // your code here
+  // Trim leading whitespace and split by space
+  s = s.trim();
+
+  // Find index of first space
+  const spaceIndex = s.indexOf(' ');
+
+  // If no space is found, return the whole string
+  if (spaceIndex === -1) {
+    return s;
+  }
+
+  // Otherwise, return substring up to the first space
+  return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
